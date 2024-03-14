32b796aca49f428d47c36065c849a203fcd8f4cc
sprout2744 <g2g2g26465@gmail.com>
2024-03-14 16:34:01 +0900

    결제시스템

--- src/components/cart/totalCart.js
+++ src/components/cart/totalCart.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 
 export const TotalCart = ({ total, setTotal, cart, convertPrice, found, effect, deps }) => {
+
     useEffect(() => {
         if (found) {
             const temp = found.filter((item) => item.length !== 0);
@@ -16,6 +17,41 @@ export const TotalCart = ({ total, setTotal, cart, convertPrice, found, effect, 
             setTotal(0);
         }
     }, [cart, total, found, setTotal]);
+
+    const onClickPayment = () => {
+        const {IMP} = window;
+        IMP.init('imp07817434');
+
+        const data = {
+            pg: 'nice_v2',
+            pay_method: 'card',
+            merchant_uid: `mid_${new Date().getTime()}`,
+            name: '결제 테스트',
+            amount: '100',
+            custom_data: {
+                name: '부가정보',
+                desc: '세부 부가정보',
+            },
+            buyer_name: '홍길동',
+            buyer_tel: '01012345678',
+            buyer_email: '14279625@gmail.com',
+            buyer_addr: '구천면로 000-00',
+            buyer_postalcode: '01234',
+        };
+
+        IMP.request_pay(data, callback);
+    };
+
+    const callback = response => {
+        const {success, error_msg, imp_uid, merchant_uid, pay_method, paid_amount, status} = response;
+
+        if (success) {
+            alert('결제 성공');
+        } else {
+            alert(`결제 실패: ${error_msg}`);
+        }
+    };
+
 
     return (
         <div className="max-w-4xl xl:max-w-7xl mb-12 h-40 bg-gray-100 rounded-xl mx-auto mt-10 grid grid-cols-7 items-center justify-items-center">
@@ -42,7 +78,7 @@ export const TotalCart = ({ total, setTotal, cart, convertPrice, found, effect, 
                 <p className="text-4xl font-bold text-red-600">{convertPrice(total)}</p>
             </div>
             <div>
-                <button className="h-10 w-28 text-white bg-green-500 rounded-xl">결제하기</button>
+                <button className="h-10 w-28 text-white bg-green-500 rounded-xl" onClick={onClickPayment}>결제하기</button>
             </div>
         </div>
     );
